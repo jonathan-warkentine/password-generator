82a4f7e434fad34121840083e0a70717eca148c3
Jonathan Warkentine <jonathan.warkentine@gmail.com>
2022-01-17 13:24:48 -0500

    created user interface slider and checkboxes, password generating algorithm in place

---
+++ README.md
@@ -0,0 +1,13 @@
+
+
+Criteria Tracking:
+
+
+WHEN asked for character types to include in the password
+THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+WHEN I answer each prompt
+THEN my input should be validated and at least one character type should be selected
+WHEN all prompts are answered
+THEN a password is generated that matches the selected criteria
+WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,21 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var pwLengthRangeEl = document.querySelector('#pw-length-range');
+var pwLengthEl = document.querySelector('#pw-length');
+
+var symbolsCheckboxEl = document.querySelector('#include-symbols');
+var lowercaseCheckboxEl = document.querySelector('#include-lowercase');
+var uppercaseCheckboxEl = document.querySelector('#include-uppercase');
+var numbersCheckboxEl = document.querySelector('#include-numbers');
+
+const symbols = "<>`~!@#$%^&*()-_=+[]{}|";
+const lowercase = "abcdefghijklmnopqrstuvwxyz";
+const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numbers = "1234567890";
+
+pwLengthEl.textContent = "Password Length: " + pwLengthRangeEl.value;
+
+
 
 // Write password to the #password input
 function writePassword() {
@@ -10,5 +26,56 @@ function writePassword() {
 
 }
 
+function pwLengthModified() {
+  
+  if (pwLengthRangeEl.value == pwLengthRangeEl.max) {
+    pwLengthEl.textContent = "Password Length: " + pwLengthRangeEl.value + " (max)";
+  }
+  else if (pwLengthRangeEl.value == pwLengthRangeEl.min) {
+    pwLengthEl.textContent = "Password Length: " + pwLengthRangeEl.value + " (min)";
+
+  }
+  else {
+    pwLengthEl.textContent = "Password Length: " + pwLengthRangeEl.value;
+  }
+
+}
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+pwLengthRangeEl.addEventListener("mousemove", pwLengthModified);
+// symbolsCheckboxEl.addEventListener("change", checkboxChange);
+
+
+function generatePassword() {
+
+  let password = "";
+  let allowableCharacters = "";
+
+  if (symbolsCheckboxEl.checked) {
+    allowableCharacters += symbols;
+  }
+
+  if (lowercaseCheckboxEl.checked) {
+    allowableCharacters += lowercase;
+  }
+
+  if (uppercaseCheckboxEl.checked) {
+    allowableCharacters += uppercase;
+  }
+
+  if (numbersCheckboxEl.checked) {
+    allowableCharacters += numbers;
+  }
+
+  if (allowableCharacters.length == 0){
+    return "No possible characters! Please check at least one box below."
+  }
+
+  for (let i=0; i<pwLengthRangeEl.value; i++){
+    password += allowableCharacters.charAt(Math.floor(Math.random() * allowableCharacters.length));
+  }
+
+  return password;
+}
