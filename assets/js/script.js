b7cca00b1c71e8bbf06cde33ed486b471a871d5c
Jonathan Warkentine <jonathan.warkentine@gmail.com>
2022-01-20 17:41:01 -0500

    fully commented the Javascript, implemented a click-to-copy mechanism

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,8 +13,17 @@ const lowercase = "abcdefghijklmnopqrstuvwxyz";
 const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const numbers = "1234567890";
 
+//This populates the label for the password length slider input
 pwLengthEl.textContent = "Password Length: " + pwLengthRangeEl.value;
 
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+//add event listener to register a change to the password length selector and update the HTML element accordingly
+pwLengthRangeEl.addEventListener("mousemove", pwLengthModified);
+
+//add event listener to copy password text if the containing box is clicked
+document.querySelector("#password").addEventListener("click", copyPassword);
 
 
 // Write password to the #password input
@@ -24,8 +33,47 @@ function writePassword() {
 
   passwordText.value = password;
 
+  //encourage user to click password to copy
+  document.querySelector("#GenPass").textContent = "Click to copy!";
+  document.querySelector("#GenPass").setAttribute("style", "text-align: center;");
 }
 
+    //function generatePassword generates a random password based on the user-inputed
+    //password length, and the allowable characters specified
+    function generatePassword() {
+
+      let password = "";
+      let allowableCharacters = "";
+
+      if (symbolsCheckboxEl.checked) {
+        allowableCharacters += symbols;
+      }
+
+      if (lowercaseCheckboxEl.checked) {
+        allowableCharacters += lowercase;
+      }
+
+      if (uppercaseCheckboxEl.checked) {
+        allowableCharacters += uppercase;
+      }
+
+      if (numbersCheckboxEl.checked) {
+        allowableCharacters += numbers;
+      }
+
+      if (allowableCharacters.length == 0){
+        return "No possible characters! Please check at least one box below."
+      }
+
+      for (let i=0; i<pwLengthRangeEl.value; i++){
+        password += allowableCharacters.charAt(Math.floor(Math.random() * allowableCharacters.length));
+      }
+
+      return password;
+    }
+
+
+//function pwLengthModified() adjusts the display of the password length field
 function pwLengthModified() {
   
   if (pwLengthRangeEl.value == pwLengthRangeEl.max) {
@@ -41,41 +89,19 @@ function pwLengthModified() {
 
 }
 
+//function copyPassword() copies the generated password from the #password element, 
+//copies to user's clipboard, and notifies the user
+function copyPassword() {
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-pwLengthRangeEl.addEventListener("mousemove", pwLengthModified);
-// symbolsCheckboxEl.addEventListener("change", checkboxChange);
-
-
-function generatePassword() {
-
-  let password = "";
-  let allowableCharacters = "";
-
-  if (symbolsCheckboxEl.checked) {
-    allowableCharacters += symbols;
+  //avoids copying a blank password field
+  if (document.querySelector("#password").value=="") {
+    return;
   }
+  
+  //copy password to clipboard
+  navigator.clipboard.writeText(document.querySelector("#password").value);
 
-  if (lowercaseCheckboxEl.checked) {
-    allowableCharacters += lowercase;
-  }
-
-  if (uppercaseCheckboxEl.checked) {
-    allowableCharacters += uppercase;
-  }
-
-  if (numbersCheckboxEl.checked) {
-    allowableCharacters += numbers;
-  }
-
-  if (allowableCharacters.length == 0){
-    return "No possible characters! Please check at least one box below."
-  }
-
-  for (let i=0; i<pwLengthRangeEl.value; i++){
-    password += allowableCharacters.charAt(Math.floor(Math.random() * allowableCharacters.length));
-  }
-
-  return password;
+  //notify user
+  document.querySelector("#GenPass").textContent = "Copied!";
+  document.querySelector("#GenPass").setAttribute("style", "text-align: center;");
 }
